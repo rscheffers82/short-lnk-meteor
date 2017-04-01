30fecb28eb38308f9ea3a5ba1940cd8d2d78cdbe
Roy Scheffers <r.scheffers@gmail.com>
2017-04-01 18:11:08 -0400

    add initial signup and login logic

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { Meteor } from 'meteor/meteor';
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: ''
+    };
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    let email = this.refs.email.value.trim();
+    let password = this.refs.password.value.trim();
+
+    Meteor.loginWithPassword({email}, password, (err) => {
+      console.log('Login callback', err);
+    });
+  }
+
   render() {
     return(
       <div>
-        <h1>Login page</h1>
-        <form>
-          <input type="text" placeholder="Username"/>
-          <input type="text" placeholder="Password" />
+        <h1>Short Lnk</h1>
+
+        {this.state.error ? <p>{this.state.error}</p> : undefined}
+
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="email" ref="email" name="email" placeholder="Email"/>
+          <input type="text" ref="email" name="password" placeholder="Password" />
           <button>Login</button>
         </form>
+
         <Link to="/signup">Want an account?</Link>
       </div>
     );
--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { Accounts } 'meteor/accounts-base';
 
 export default class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: ''
+    };
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    let email = this.refs.email.value.trim();
+    let password = this.refs.password.value.trim();
+
+    Accounts.createUser({email , password}, (err) => {
+      console.log('Singup callback', err);
+    });
+
+    // this.setState({
+    //   error: 'Something went wrong.'
+    // });
+  }
+
   render() {
     return(
       <div>
-        <h1>Signup page</h1>
-        <form>
-          <input type="text" placeholder="Username"/>
-          <input type="text" placeholder="Password" />
-          <button>Signup</button>
+        <h1>Join Short Lnk</h1>
+        {this.state.error ? <p>{this.state.error}</p> : undefined}
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="email" ref="email" name="email" placeholder="Email"/>
+          <input type="text" ref="email" name="password" placeholder="Password" />
+          <button>Create Account</button>
         </form>
         <Link to="/">Already have an account?</Link>
       </div>
