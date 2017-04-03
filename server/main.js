8dacac6975257dc5dd604009b9585a2f5bcf3001
Roy Scheffers <r.scheffers@gmail.com>
2017-04-02 21:48:09 -0400

    refactor router code

--- server/main.js
+++ server/main.js
@@ -1,31 +1,10 @@
 import { Meteor } from 'meteor/meteor';
-import SimpleSchema from 'simpl-schema';
-import { Accounts } from 'meteor/accounts-base'
+
+import '../imports/api/users';
+
 
 Meteor.startup(() => {
   // code to run on server at startup
-
-  Accounts.validateNewUser((user) => {
-    const email = user.emails[0].address;
-
-    try {
-      new SimpleSchema({
-        email: {
-          type: String,
-          regEx: SimpleSchema.RegEx.Email
-        }
-      }).validate({ email });
-    } catch(e) {
-      throw new Meteor.Error(400, e.message);
-    }
-
-    console.log('user: ', user);
-    return true;
-  });
-
-
-
-
 
   // -- SimpleSchema example
   // const employeeSchema = new SimpleSchema({
