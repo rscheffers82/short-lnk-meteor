b7b0f0cdeb54f62a6d073305f38a979fa5379aec
Roy Scheffers <r.scheffers@gmail.com>
2017-04-11 11:12:22 -0400

    Add clipboard copy to the project

--- imports/ui/LinkListItem.js
+++ imports/ui/LinkListItem.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
+import Clipboard from 'clipboard';
 
 export default class LinkListItem extends Component {
+  componentDidMount() {
+    this.clipboard = new Clipboard(this.refs.copy);
+
+    this.clipboard.on('success', () => {
+      alert('it works!');
+    }).on('error', () => {
+      alert('Unable to copy. Please manually copy the link.')
+    })
+  }
+
+  componentWillUnmount() {
+    this.clipboard.destroy();
+  }
+
   render() {
     const { shortUrl, url } = this.props
     return (
       <div>
-        <p>
-          url: {url}
-        </p>
-        <p>
-          shortUrl: {shortUrl}
-        </p>
+        <p>url: {url}</p>
+        <p>shortUrl: {shortUrl}</p>
+        <button ref="copy" data-clipboard-text={shortUrl}>Copy</button>
       </div>
     );
   }
